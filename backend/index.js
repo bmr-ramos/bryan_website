e3c9a7d72c2f97a62104916885b11c0e7b917ab8
br344737 <br344737@gmail.com>
2024-05-17 19:41:56 -0500

    5/17/24 Build 3

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
